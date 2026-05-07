c68ae287b205e40b973c09a701f5681ed25549c0
Pradeep Neelaboyina <Pradeepdevops81@gmail.com>
2026-05-08 01:43:59 +0200

    PETC-INF-01: add Terraform ACR setup and update .gitignore

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "project_prefix" {
+  description = "petclinic11"
+  type        = string
+}
+variable "location" {
+  type    = string
+  default = "westeurope"
+}
+variable "node_count" {
+  type    = number
+  default = 2
+}
+variable "node_vm_size" {
+  type    = string
+  default = "Standard_B2s_v2"  
+}
